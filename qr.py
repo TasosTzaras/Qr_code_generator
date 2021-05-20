ad8aa2f424f7910938615f59172db071c984231e
Tasos Tzaras <51204291+TasosTzaras@users.noreply.github.com>
2021-05-21 02:43:21 +0300

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 import qrcode
-import time
 
 # s = https://github.com/TasosTzaras
 s = input("give the url to QRCode: \n")
 img = qrcode.make(s)
-time.sleep(5)
 img.save("qr.png")
 print("QRCode generated successfully!!")
